2504a7600401a5897881b66aa614018e681aac19
Alex Church <34soccerplayer@gmail.com>
2015-01-17 17:10:24 -0500

    Initial Setup version 2

--- src/Heuristic.java
+++ src/Heuristic.java
@@ -1,4 +1,7 @@
 
-public class Heuristic {
-
+public class Heuristic 
+{
+	//Add Heuristic function with a board state as an input and an integer value as an output
+	//Add Heuristic helper methods
+	//Todo: Add multiple Heuristic levels based on time remaining
 }
