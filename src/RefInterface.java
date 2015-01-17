83f11a4c66b6ef1d41f8c4030d5b0d04dfeb204a
Alex Church <34soccerplayer@gmail.com>
2015-01-17 17:07:50 -0500

    Initial Setup

---
+++ src/RefInterface.java
@@ -0,0 +1,38 @@
+
+public class RefInterface 
+{
+	int playerNumber = 0;
+	
+	public void announce()
+	{
+		//This should send the Ref an identity and should update which player number we are
+	}
+	public void gameStart()
+	{
+		//This should get the game information from the Ref
+		/*
+		 * Game information consists of 5 numbers [in this order]: 
+		 * board height (#rows), 
+		 * board width (#columns), 
+		 * number of pieces to win (the N in Connect-N), 
+		 * turn of the player (1 indicating 1st player, and 2 indicating 2nd player), 
+		 * and the time limit to make a move in seconds. 
+		 * 
+		 * Once the players receive these information, the game starts immediately. 
+		 * These are sent as a one line separated with spaces.
+		 */
+	}
+	
+	public void makeMove(int column, int movetype)
+	{
+		//This should tell the Ref which column to put the piece into and if it is a popout or a normal move 
+	}
+	
+	public void updateBoard()
+	{
+		//This should read the Ref's output as to where the other player went
+	}
+
+	//ToDo: Add Timer
+	//ToDo: Add Ref interface
+}
